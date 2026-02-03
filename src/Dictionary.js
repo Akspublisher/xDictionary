04639de95c7870910eb43017886940ce3070490c
hp <sudesh7678582882@gmail.com>
2026-02-03 21:31:50 +0530

    Update commit

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,45 @@
-
+import React, { useState } from 'react';
 
 export default function Dictionary() {
 
-  return  (
-    <div>
-        <h1>Dictionary Component</h1>
-    </div>
-  )}
+    const [inputWord, setInputWord] = useState("");
+    const [definition, setDefinition] = useState("");
+
+    const dictionary = 
+[
+
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." }
+
+]
+
+const handleSearch = (e) => {
+    //alert("Search button ok");     
+    e.preventDefault();
+    if(inputWord === "") {
+        alert("Please enter a word to search");
+        return;
+    }
+    const foundWord = dictionary.find(word => word.word.toLowerCase() === inputWord.toLowerCase());
+ //   alert("Found word: " + JSON.stringify(foundWord));
+    if (foundWord) {
+        setDefinition(foundWord.meaning);
+   //     alert("Definition: " + foundWord.meaning);
+    } else {
+        setDefinition("Word not found in the dictionary.");
+    }
+}
+
+    return (
+        <div>
+            <h1 style={{marginBottom:"10px"}}>Dictionary App</h1>
+            <input type ="search" placeholder ="search for a word"  onChange={(e) => setInputWord(e.target.value)} />
+            <button onClick={handleSearch}>Search</button>
+            <h3>Definition:</h3>
+            <p>{definition}</p>
+        </div>
+    )
+  }
